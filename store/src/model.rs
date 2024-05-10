fa87fb12587e4048d5f328674c07a325c4ef483c
dg <garciad48879@gmail.com>
2024-05-10 19:08:17 -0400

    store: update model

--- store/src/model.rs
+++ store/src/model.rs
@@ -1,7 +1,34 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Widget {
     pub(crate) id: i32,
-    pub(crate) name: &'static str
+    pub(crate) name: &'static str,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Customer {
+    pub id: i32,
+    pub create_date: Option<DateTime<Utc>>,
+    pub first_name: &'static str,
+    pub last_name: &'static str,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Product {
+    pub id: i32,
+    pub create_date: Option<DateTime<Utc>>,
+    pub product_name: &'static str,
+    pub cost: &'static str,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Order {
+    pub id: i32,
+    pub create_date: Option<DateTime<Utc>>,
+    pub customer_id: i32,
+    pub product_id: i32,
+    pub paid_date: Option<DateTime<Utc>>,
+    pub shipped_date: Option<DateTime<Utc>>,
 }
