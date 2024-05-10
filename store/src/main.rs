a45817b015cf4e372c526886b2b0e782c3cb06f3
dg <garciad48879@gmail.com>
2024-05-10 19:21:20 -0400

    store: add future endpoints

--- store/src/main.rs
+++ store/src/main.rs
@@ -15,7 +15,39 @@ fn widgets() -> Json<Vec<Widget>> {
     Json(widgets)
 }
 
+#[get("/customer/shopping/products")]
+fn customer_shopping_products() -> Json<Vec<String>> {
+    Json(vec!("replace-me".to_string()))
+}
+
+#[get("/employee/manufacturing/pending")]
+fn employee_manufacturing_pending() -> Json<Vec<String>> {
+    Json(vec!("replace-me".to_string()))
+}
+
+#[get("/employee/manufacturing/open")]
+fn employee_manufacturing_open() -> Json<Vec<String>> {
+    Json(vec!("replace-me".to_string()))
+}
+
+#[get("/employee/manufacturing/completed")]
+fn employee_manufacturing_completed() -> Json<Vec<String>> {
+    Json(vec!("replace-me".to_string()))
+}
+
+#[get("/employee/management/reports/orders")]
+fn employee_management_reports_orders() -> Json<Vec<String>> {
+    Json(vec!("replace-me".to_string()))
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/store", routes![widgets])
+    rocket::build().mount("/", routes![
+        widgets,
+        customer_shopping_products,
+        employee_manufacturing_pending,
+        employee_manufacturing_open,
+        employee_manufacturing_completed,
+        employee_management_reports_orders,
+    ])
 }
